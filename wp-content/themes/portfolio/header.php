9061257744075e9362fe889fca0c3f05b59dce5e
scottjparker21 <scottjparker21@gmail.com>
2017-02-03 17:56:36 -0600

    added materialize

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -12,6 +12,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
+		<!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 		<?php wp_head(); ?>
 		<script>
         // conditionizr.com
--- wp-content/themes/portfolio/page.php
+++ wp-content/themes/portfolio/page.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_header( home ); ?>
 
 	<main role="main">
 		<!-- section -->
@@ -12,8 +12,6 @@
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
 
 				<br class="clear">
 
